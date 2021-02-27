bfe2e45dca586f1d08efc21b4def293b5830b6e2
deskull <deskull@users.sourceforge.jp>
2021-02-27 23:42:13 +0900

    [Feature] ＠さんがなんでも食えるいい子に。 / Player can eat anything.

--- src/sv-definition/sv-junk-definition.h
+++ src/sv-definition/sv-junk-definition.h
@@ -4,7 +4,7 @@
 typedef enum sv_junk_type {
     SV_JUNK_SHARD_OF_POTTERY = 0,
     SV_JUNK_BROKEN_STICK  = 1,
-    SV_JUNK_FECES = 2,
-    SV_JUNK_VOMITTING = 3,
-    SV_JUNK_THING_LIVING_TILL = 4,
+    SV_JUNK_FECES = 7,
+    SV_JUNK_VOMITTING = 8,
+    SV_JUNK_THING_LIVING_TILL = 9,
 } sv_junk_type;
