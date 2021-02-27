e023f6ee475b2f6b7b008679b55f65d0b116e56a
deskull <deskull@users.sourceforge.jp>
2021-02-27 23:27:38 +0900

    [Add] Add sv-junk-definition.h.

---
+++ src/sv-definition/sv-junk-definition.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+/* The "sval" codes for TV_JUNK */
+typedef enum sv_junk_type {
+    SV_JUNK_SHARD_OF_POTTERY = 0,
+    SV_JUNK_BROKEN_STICK  = 1,
+    SV_JUNK_FECES = 2,
+    SV_JUNK_VOMITTING = 3,
+    SV_JUNK_THING_LIVING_TILL = 4,
+} sv_junk_type;
