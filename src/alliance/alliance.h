254a83b62421c7437ac5f0acac0cc04cee537477
Deskull <61610939+sikabane-works@users.noreply.github.com>
2021-05-28 23:15:48 +0900

    [Implement] カオスフレームを「アライアンス」と命名の上実装開始．まずクラス作成．

---
+++ src/alliance/alliance.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "system/angband.h"
+#include "util/flag-group.h"
+#include <string>
+#include <vector>
+
+typedef int ALLIANCE_ID;
+
+enum alliance_flags
+{
+    ALLF_ORDER, //!< 秩序の陣営
+    ALLF_CHAOS,  //!< 混沌の陣営
+    MAX,
+};
+
+class alliance {
+public:
+    std::string name; //!< 陣営名
+    // = FlagGroup<FlagType, FlagType::MAX>;
+    EnumClassFlagGroup<alliance_flags> alliFlags; //!< 陣営特性フラグ
+};
+
+std::vector<alliance> alliance_list;
