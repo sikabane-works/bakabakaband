7d814cf5f313a85101c1a4e12840fbf6ef63b718
Deskull <61610939+sikabane-works@users.noreply.github.com>
2021-06-21 22:51:55 +0900

    [Implement] 「これまで行った行為の一覧」を実装．

---
+++ src/knowledge/knowledge-incident.cpp
@@ -0,0 +1,25 @@
+﻿/*!
+ * @brief これまでの行いを表示する
+ * @date 2021/06/21
+ * @author Deskull
+ */
+
+#include "core/show-file.h"
+#include "io-dump/dump-util.h"
+#include "system/player-type-definition.h"
+#include "util/angband-files.h"
+#include <string>
+
+void do_cmd_knowledge_incident(player_type *creature_ptr)
+{
+    FILE *fff = NULL;
+    GAME_TEXT file_name[FILE_NAME_SIZE];
+    if (!open_temporary_file(&fff, file_name))
+        return;
+
+    fprintf(fff, _("貴方はこれまで%d歩進んだ。\n", "You walked %d steps\n"), creature_ptr->incident[INCIDENT::WALK]);
+    angband_fclose(fff);
+    (void)show_file(creature_ptr, true, file_name, _("これまでの出来事", "Incidents"), 0, 0);
+    fd_kill(file_name);
+
+}
---
+++ src/knowledge/knowledge-incident.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+
+typedef struct player_type player_type;
+void do_cmd_knowledge_incident(player_type *creature_ptr);
