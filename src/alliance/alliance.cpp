2707ea5a6b373229ffe743830adf431b8cc6e879
deskull <deskull@users.sourceforge.jp>
2021-08-21 13:43:12 +0900

    [Implement] 4勢力のプレイヤーのアライメントに応じた印象値計算追加.

--- src/alliance/alliance.cpp
+++ src/alliance/alliance.cpp
@@ -1,25 +1,22 @@
 ﻿#include "alliance/alliance.h"
+#include "system/player-type-definition.h"
 
 int AllianceAmber::calcImplessionPoint(player_type* creature_ptr)
 {
-    (creature_ptr);
-    return 0;
+    return (creature_ptr->alignment > 0) ? creature_ptr->alignment / 3 : creature_ptr->alignment;
 }
 
 int AllianceCourtOfChaos::calcImplessionPoint(player_type *creature_ptr)
 {
-    (creature_ptr);
-    return 0;
+    return (creature_ptr->alignment > 0) ? creature_ptr->alignment / 3 : -creature_ptr->alignment / 2;
 }
 
 int AllianceValinor::calcImplessionPoint(player_type *creature_ptr)
 {
-    (creature_ptr);
-    return 0;
+    return (creature_ptr->alignment > 0) ? creature_ptr->alignment : -creature_ptr->alignment * 2;
 }
 
 int AllianceUtumno::calcImplessionPoint(player_type *creature_ptr)
 {
-    (creature_ptr);
-    return 0;
+    return (creature_ptr->alignment > 0) ? creature_ptr->alignment / 3 : -creature_ptr->alignment / 2;
 }
