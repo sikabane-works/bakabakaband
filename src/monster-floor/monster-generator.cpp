5f6c9c38c80b0ac644c91eff19514d0017a550a2
deskull <deskull@users.sourceforge.jp>
2021-09-01 23:14:23 +0900

    [Refactor] 後続生成数のデータ構造定義をvector&tupleに変更．

--- src/monster-floor/monster-generator.cpp
+++ src/monster-floor/monster-generator.cpp
@@ -278,17 +278,20 @@ bool place_monster_aux(player_type *player_ptr, MONSTER_IDX who, POSITION y, POS
     place_monster_m_idx = hack_m_idx_ii;
 
     /* Reinforcement */
-    for (int i = 0; i < 6; i++) {
-        if (!r_ptr->reinforce_id[i])
+    for (auto reinforce : r_ptr->reinforces) {
+        auto mon_idx = std::get<0>(reinforce);
+        auto dn = std::get<1>(reinforce);
+        auto ds = std::get<2>(reinforce);
+        if (!mon_idx)
             break;
-        int n = damroll(r_ptr->reinforce_dd[i], r_ptr->reinforce_ds[i]);
+        int n = damroll(dn, ds);
         for (int j = 0; j < n; j++) {
             POSITION nx, ny, d;
             const POSITION scatter_min = 7;
             const POSITION scatter_max = 40;
             for (d = scatter_min; d <= scatter_max; d++) {
                 scatter(player_ptr, &ny, &nx, y, x, d, PROJECT_NONE);
-                if (place_monster_one(player_ptr, place_monster_m_idx, ny, nx, r_ptr->reinforce_id[i], mode)) {
+                if (place_monster_one(player_ptr, place_monster_m_idx, ny, nx, mon_idx, mode)) {
                     break;
                 }
             }
