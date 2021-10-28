e50b5b64a61d9e92e57f82f85cba594c7d07ab60
Deskull <61610939+sikabane-works@users.noreply.github.com>
2021-10-28 22:46:29 +0900

    [System][Refactor] 時空崩壊度とその処理を独立クラスに移行。

---
+++ src/world/world-collapsion.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <cstdint>
+
+class WorldCollapsion {
+public:
+    WorldCollapsion();
+    int32_t collapse_degree{}; /*!< 時空崩壊度 */
+};
+
+extern WorldCollapsion world_collapsion;
+extern WorldCollapsion *wc_ptr;
