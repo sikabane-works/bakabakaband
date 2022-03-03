bae77e34d62a4b7bc21e3ce0fbc80f5e33b2627c
Habu <habu1010+github@gmail.com>
2022-03-03 23:44:59 +0900

    [Refactor] MonsterRace クラスを導入
    monster_race のデータに関連する処理を担う MonsterRace クラスを導入する。
    まず is_valid() を実装し、有効なモンスターかどうかを調べる r_idx と
    MonsterRaceId::PLAYER との比較を置き換える。

--- src/monster-race/monster-race.h
+++ src/monster-race/monster-race.h
@@ -10,3 +10,12 @@ extern std::map<MonsterRaceId, monster_race> r_info;
 int calc_monrace_power(monster_race *r_ptr);
 int calc_monrace_eval(monster_race *r_ptr);
 bool is_valid_monster_race(MonsterRaceId r_idx);
+
+class MonsterRace {
+public:
+    MonsterRace(MonsterRaceId r_idx);
+    bool is_valid() const;
+
+private:
+    const MonsterRaceId r_idx;
+};
