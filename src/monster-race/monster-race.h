94eab831e4fad23c5a867a0ce6fa067c8ba16e57
Habu <habu1010+github@gmail.com>
2022-03-06 00:28:24 +0900

    [Refactor] calc_monrace_power() を MonsterRace のメンバ関数にする(+馬鹿馬鹿の戦力指数も織り込む)
    役割からして MonsterRace クラスのメンバ関数であるべき。
    また、これを使用しているモンスター闘技場からの呼び出し部分を STL を使って書き直し、
    battle_mon 配列を複数なので battle_mon_list に改名する。

--- src/monster-race/monster-race.h
+++ src/monster-race/monster-race.h
@@ -7,9 +7,6 @@
 enum class MonsterRaceId : int16_t;
 struct monster_race;
 extern std::map<MonsterRaceId, monster_race> r_info;
-int calc_monrace_power(monster_race *r_ptr);
-int calc_monrace_eval(monster_race *r_ptr);
-bool is_valid_monster_race(MonsterRaceId r_idx);
 
 class MonsterRace {
 public:
@@ -18,6 +15,8 @@ public:
     static MonsterRaceId pick_one_at_random();
 
     bool is_valid() const;
+    int calc_eval() const;
+    int calc_power() const;
 
 private:
     const MonsterRaceId r_idx;
