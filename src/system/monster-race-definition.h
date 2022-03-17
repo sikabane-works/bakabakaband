46c7e21ad06d8a638ceb6622cdfb7c4808d1118b
Habu <habu1010+github@gmail.com>
2022-03-18 00:19:04 +0900

    [Fix] 特定アーティファクトドロップのデータ数の制限をなくす
    特定アーティファクトドロップのデータを std::vector で持つようにし、1体につき4種まで
    の制限をなくす。
    ほとんどのモンスターは特定アーティファクトドロップを持たないので、無駄なメモリ消費の削
    減にもなる。
    また、r_info.txt 上の特定アーティファクトドロップの定義が
    A:artifact_idx:rarity:chance
    となっているが、rarity はゲーム内で使用されておらず、紛らわしいので削除する。

--- src/system/monster-race-definition.h
+++ src/system/monster-race-definition.h
@@ -88,9 +88,8 @@ struct monster_race {
     std::vector<std::tuple<int, int, KIND_OBJECT_IDX, int, int, int>> drop_kinds; //!< アイテム特定ドロップ指定
     std::vector<std::tuple<int, int, KIND_OBJECT_IDX, int, int, int>> drop_tvals; //!< アイテム種別ドロップ指定
     std::vector<std::tuple<int, int, MonsterRaceId, int, int>> dead_spawns; //!< 死亡時モンスター生成
-    ARTIFACT_IDX artifact_id[4]{}; //!< 特定アーティファクトドロップID
-    RARITY artifact_rarity[4]{}; //!< 特定アーティファクトレア度
-    PERCENTAGE artifact_percent[4]{}; //!< 特定アーティファクトドロップ率
+    //! 特定アーティファクトドロップリスト <アーティファクトID,ドロップ率>
+    std::vector<std::tuple<ARTIFACT_IDX, PERCENTAGE>> drop_artifacts;
     PERCENTAGE arena_ratio{}; //!< モンスター闘技場の掛け金倍率修正値(%基準 / 0=100%) / The adjustment ratio for gambling monster
     MonsterRaceId next_r_idx{}; //!< 進化先モンスター種族ID
     EXP next_exp{}; //!< 進化に必要な経験値
