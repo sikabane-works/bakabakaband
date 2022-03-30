e5789ea7c83ec21034350fcf930dc69729c6b289
Slimebreath6078 <slimebreath6078@yahoo.co.jp>
2022-03-30 20:19:17 +0900

    [Refactor] ビット判定・操作式を ***_bits() にした

--- src/io/write-diary.cpp
+++ src/io/write-diary.cpp
@@ -16,6 +16,7 @@
 #include "system/monster-race-definition.h"
 #include "system/player-type-definition.h"
 #include "util/angband-files.h"
+#include "util/bit-flags-calculator.h"
 #include "view/display-messages.h"
 #include "world/world.h"
 #include <ctime>
@@ -199,7 +200,7 @@ int exe_write_diary_quest(PlayerType *player_ptr, int type, QuestId num)
 
     switch (type) {
     case DIARY_FIX_QUEST_C: {
-        if (q_ref.flags & QUEST_FLAG_SILENT) {
+        if (any_bits(q_ref.flags, QUEST_FLAG_SILENT)) {
             break;
         }
 
@@ -207,7 +208,7 @@ int exe_write_diary_quest(PlayerType *player_ptr, int type, QuestId num)
         break;
     }
     case DIARY_FIX_QUEST_F: {
-        if (q_ref.flags & QUEST_FLAG_SILENT) {
+        if (any_bits(q_ref.flags, QUEST_FLAG_SILENT)) {
             break;
         }
 
@@ -227,7 +228,7 @@ int exe_write_diary_quest(PlayerType *player_ptr, int type, QuestId num)
         break;
     }
     case DIARY_TO_QUEST: {
-        if (q_ref.flags & QUEST_FLAG_SILENT) {
+        if (any_bits(q_ref.flags, QUEST_FLAG_SILENT)) {
             break;
         }
 
