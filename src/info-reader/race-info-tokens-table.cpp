21891667c1cb2bb442b0623bfc19c2a0cb2d3da7
Deskull <sikabane-works@users.noreply.github.com>
2022-09-22 23:05:26 +0900

    [Fix] プリーストの部族表示抜けを修正。

--- src/info-reader/race-info-tokens-table.cpp
+++ src/info-reader/race-info-tokens-table.cpp
@@ -299,7 +299,6 @@ const std::unordered_map<std::string_view, race_flags8> r_info_flags8 = {
     { "WILD_MOUNTAIN", RF8_WILD_MOUNTAIN },
     { "WILD_GRASS", RF8_WILD_GRASS },
     { "QUYLTHLUG", RF8_QUYLTHLUG },
-    { "PRIEST", RF8_PRIEST },
     { "PALADIN", RF8_PALADIN },
     { "RANGER", RF8_RANGER },
     { "SAMURAI", RF8_SAMURAI },
@@ -489,4 +488,5 @@ const std::unordered_map<std::string_view, MonsterKindType> r_info_kind_flags = 
     { "WARRIOR", MonsterKindType::WARRIOR },
     { "ROGUE", MonsterKindType::ROGUE },
     { "MAGE", MonsterKindType::MAGE },
+    { "PRIEST", MonsterKindType::PRIEST },
 };
