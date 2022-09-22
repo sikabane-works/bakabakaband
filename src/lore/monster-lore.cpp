ec77770ed5fde858bc034138e5a752e9b9a31578
Deskull <sikabane-works@users.noreply.github.com>
2022-09-22 12:53:35 +0900

    [Fix] 盗賊の部族表示抜けを修正。

--- src/lore/monster-lore.cpp
+++ src/lore/monster-lore.cpp
@@ -142,6 +142,10 @@ static void set_race_flags(lore_type *lore_ptr)
         lore_ptr->kind_flags.set(MonsterKindType::WARRIOR);
     }
 
+    if (lore_ptr->r_ptr->kind_flags.has(MonsterKindType::ROGUE)) {
+        lore_ptr->kind_flags.set(MonsterKindType::ROGUE);
+    }
+
     if (lore_ptr->r_ptr->flags1 & RF1_FORCE_DEPTH) {
         lore_ptr->flags1 |= (RF1_FORCE_DEPTH);
     }
