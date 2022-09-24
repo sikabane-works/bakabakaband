cc0b846fcc14114c4e44bd4859e9a62d916aa583
Deskull <sikabane-works@users.noreply.github.com>
2022-09-24 13:21:33 +0900

    [Fix] ニンジャの部族表示抜けを修正。

--- src/lore/monster-lore.cpp
+++ src/lore/monster-lore.cpp
@@ -166,6 +166,10 @@ static void set_race_flags(lore_type *lore_ptr)
         lore_ptr->kind_flags.set(MonsterKindType::SAMURAI);
     }
 
+    if (lore_ptr->r_ptr->kind_flags.has(MonsterKindType::NINJA)) {
+        lore_ptr->kind_flags.set(MonsterKindType::NINJA);
+    }
+
     if (lore_ptr->r_ptr->flags1 & RF1_FORCE_DEPTH) {
         lore_ptr->flags1 |= (RF1_FORCE_DEPTH);
     }
