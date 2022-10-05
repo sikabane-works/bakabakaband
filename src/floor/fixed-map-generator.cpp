d30fb31e4cab52f8c29fa78d3529d8277e291ea8
Deskull <sikabane-works@users.noreply.github.com>
2022-10-05 21:30:20 +0900

    [Implement] 町の固定Vaultパース処理追加。

--- src/floor/fixed-map-generator.cpp
+++ src/floor/fixed-map-generator.cpp
@@ -482,7 +482,7 @@ parse_error_type generate_fixed_map_floor(PlayerType *player_ptr, qtwg_type *qtw
     }
 
     if (qtwg_ptr->buf[0] == 'V') {
-        return parse_line_vault(player_ptr->current_floor_ptr, qtwg_ptr->buf);
+        return parse_line_vault(qtwg_ptr->buf);
     }
 
     if (parse_qtw_M(qtwg_ptr, zz)) {
--- src/info-reader/general-parser.cpp
+++ src/info-reader/general-parser.cpp
@@ -10,6 +10,7 @@
 #include "object-enchant/trg-types.h"
 #include "object/object-kind-hook.h"
 #include "realm/realm-types.h"
+#include "room/vault-builder.h"
 #include "system/artifact-type-definition.h"
 #include "system/building-type-definition.h"
 #include "system/floor-type-definition.h"
@@ -19,8 +20,9 @@
 #include <string>
 
 dungeon_grid letter[255];
+std::vector<town_vault> vault_list;
 
-/*!
+    /*!
  * @brief パース関数に基づいてデータファイルからデータを読み取る /
  * Initialize an "*_info" array, by parsing an ascii "template" file
  * @param fp 読み取りに使うファイルポインタ
@@ -65,8 +67,29 @@ errr init_info_txt(FILE *fp, char *buf, angband_header *head, std::function<errr
     return 0;
 }
 
-parse_error_type parse_line_vault(floor_type *floor_ptr, char *buf)
+/*!
+ * @brief 地形情報の「V:」情報をパースする
+ * Process "F:<ID>:<y>:<x>:<offsety>:<offsetx>:<transno>" -- info for vault_setting
+ * @param floor_ptr 現在フロアへの参照ポインタ
+ * @param buf 解析文字列
+ * @return エラーコード
+ */
+parse_error_type parse_line_vault(char *buf)
 {
+    char *zz[6];
+    town_vault tv;
+    int num = tokenize(buf + 2, 6, zz, 0);
+    if (num != 6) {
+        return PARSE_ERROR_GENERIC;
+    }
+
+    tv.id = static_cast<VaultTypeId>(atoi(zz[0]));
+    tv.y = atoi(zz[1]);
+    tv.x = atoi(zz[2]);
+    tv.yoffset = atoi(zz[3]);
+    tv.xoffset = atoi(zz[4]);
+    tv.transno = atoi(zz[5]);
+    vault_list.push_back(tv);
     return PARSE_ERROR_NONE;
 }
 
