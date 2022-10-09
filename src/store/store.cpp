901d7e11a0d65b811a37688724d822e4a6420933
Hourier <66951241+Hourier@users.noreply.github.com>
2022-10-09 15:33:14 +0900

    [Refactor] #2694 object_value() をget_price() に、object_value_base() をget_baseitem_price() に、それぞれ差し替えた

--- src/store/store.cpp
+++ src/store/store.cpp
@@ -317,11 +317,11 @@ void store_create(PlayerType *player_ptr, KIND_OBJECT_IDX fix_k_idx, StoreSaleTy
         }
 
         if (store_num == StoreSaleType::BLACK) {
-            if (black_market_crap(player_ptr, q_ptr) || (q_ptr->object_value() < 10)) {
+            if (black_market_crap(player_ptr, q_ptr) || (q_ptr->get_price() < 10)) {
                 continue;
             }
         } else {
-            if (q_ptr->object_value() <= 0) {
+            if (q_ptr->get_price() <= 0) {
                 continue;
             }
         }
