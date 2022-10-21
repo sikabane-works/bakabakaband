hengband/hengband

[Refactor] #2228 gf_descを静的配列からvectorに変えた

src/io/interpret-pref-file.cpp
@@ -337,11 +337,11 @@ static errr interpret_z_token(char *buf)
         return 1;
 
     *(t++) = '\0';
-    for (int i = 0; i < MAX_NAMED_NUM; i++) {
-        if (!streq(gf_desc[i].name, buf + 2))
+    for (const auto &desc : gf_desc) {
+        if (!streq(desc.name, buf + 2))
             continue;
 
-        gf_color[(int)gf_desc[i].num] = (TERM_COLOR)quark_add(t);
+        gf_color[(int)desc.num] = (TERM_COLOR)quark_add(t);
         return 0;
     }
 

sikabane-works/bakabakaband

src/io/interpret-pref-file.cpp
@@ -363,7 +363,7 @@ static int interpret_z_token(char *buf)
     }
 
     *(t++) = '\0';
-    for (const auto &description : gf_descriptions) {
+    for (const auto &description : gf_desc) {
         if (!streq(description.name, buf + 2)) {
             continue;
         }