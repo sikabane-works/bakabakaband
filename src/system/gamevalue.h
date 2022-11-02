8e1076306e3519c17d1bcf6f0e97b57601b68653
Hourier <66951241+Hourier@users.noreply.github.com>
2022-11-02 23:05:18 +0900

    [Refactor] #2680 守りと爆発のルーンに関する定数をmonster-move.h に移した

--- src/system/gamevalue.h
+++ src/system/gamevalue.h
@@ -26,8 +26,6 @@ constexpr int HUGE_DUNGEON_RATE = 10; /*! @brief クソデカダンジョン生�
 #define TOWN_DAWN 10000 /*!< 1日分のターン / Number of ticks from dawn to dawn XXX */
 #define TURNS_PER_TICK 10L /*!< 時間経過処理を行うターン数の刻み / Number of energy-gain-turns per ticks */
 #define MAX_DAYS 20000 /*!< 内部処理中で保持される最大日数 / Maximum days */
-#define BREAK_RUNE_PROTECTION 550 /*!< 守りのルーンの強靭度 / Rune of protection resistance */
-#define BREAK_RUNE_EXPLOSION 299 /*!< 爆発のルーンの発動しやすさ / For explosive runes */
 #define BTH_PLUS_ADJ 3 /*!< 武器経験値及びプレイヤーの打撃/射撃能力に応じた修正値倍率 / Adjust BTH per plus-to-hit */
 #define MON_SUMMON_ADJ 2 /*!< 現在未使用 Adjust level of summoned creatures */
 #define MON_DRAIN_LIFE 2 /*!< モンスターの打撃によるプレイヤーの経験値吸収基本倍率(%) / Percent of player exp drained per hit */
