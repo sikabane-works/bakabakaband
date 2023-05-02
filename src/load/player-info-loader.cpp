480c8ad8d7b16c3ea167a7409cd4c7f4d938866b
Deskull <sikabane-works@users.noreply.github.com>
2023-05-02 22:23:09 +0900

    [Refactor] rd_alter_reality() 整理。

--- src/load/player-info-loader.cpp
+++ src/load/player-info-loader.cpp
@@ -375,7 +375,8 @@ static void rd_player_status(PlayerType *player_ptr)
     player_ptr->blessed = rd_s16b();
     player_ptr->tim_invis = rd_s16b();
     player_ptr->word_recall = rd_s16b();
-    rd_alter_reality(player_ptr);
+    player_ptr->recall_dungeon = rd_s16b();
+    player_ptr->alter_reality = rd_s16b();
     player_ptr->see_infra = rd_s16b();
     player_ptr->tim_infra = rd_s16b();
     player_ptr->oppose_fire = rd_s16b();
