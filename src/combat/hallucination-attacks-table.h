23765d5c76422127c9aba1c03c165f0b9306e385
Deskull <sikabane-works@users.noreply.github.com>
2023-11-05 14:28:53 +0900

    (Flavor)[Add] 幻覚攻撃追加。

--- src/combat/hallucination-attacks-table.h
+++ src/combat/hallucination-attacks-table.h
@@ -3,7 +3,7 @@
 #include "system/angband.h"
 
 // 幻覚時のジョークメッセージ最大数 / Hallucination stuff.
-#define MAX_SILLY_ATTACK _(41, 29)
+#define MAX_SILLY_ATTACK _(43, 29)
 
 extern const concptr silly_attacks[MAX_SILLY_ATTACK];
 #ifdef JP
