36a1055f254111dbbb89b4609616297aeb41711c
Hourier <66951241+Hourier@users.noreply.github.com>
2023-11-29 19:53:42 +0900

    [Refactor] #3766 activation_explanation() をグローバル関数からItemEntity のオブジェクトメソッドへ移した

--- src/object/object-info.h
+++ src/object/object-info.h
@@ -2,15 +2,14 @@
 
 #include "object/tval-types.h"
 #include "system/angband.h"
-#include <string>
 
 #define OBJ_GOLD_LIST 480 /* First "gold" entry */
 
 class BaseitemKey;
 class ItemEntity;
 class PlayerType;
-std::string activation_explanation(const ItemEntity *o_ptr);
 char index_to_label(int i);
 int16_t wield_slot(PlayerType *player_ptr, const ItemEntity *o_ptr);
 bool check_book_realm(PlayerType *player_ptr, const BaseitemKey &bi_key);
 ItemEntity *ref_item(PlayerType *player_ptr, INVENTORY_IDX i_idx);
+std::string activation_explanation(const ItemEntity *o_ptr);
