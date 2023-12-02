hengband/hengband

[Refactor] 特性表示記号テーブルの例外フラグを std::optional にする
特性表示記号テーブルには、上位の特性を持っているときに下位の特性の表示を省略するのに
使用される例外フラグ(except_flag)メンバがあるが、例外フラグを持たない時を-1として
扱っており汚いので std::optional<tr_type> 型に変更し、例外フラグを持たない時は
std::nullopt を設定する。
また、except_flag のデフォルト引数を std::nullopt とするコンストラクタを追加して
例外フラグを持たない項目でいちいち std::nullopt を指定しなくてよいようにする。

src/flavor/flavor-util.cpp
@@ -154,7 +154,7 @@ static char *inscribe_flags_aux(std::vector<flag_insc_table> &fi_vec, const TrFl
 #endif
 
     for (flag_insc_table &fi : fi_vec) {
-        if (flgs.has(fi.flag) && (fi.except_flag == -1 || flgs.has_not(i2enum<tr_type>(fi.except_flag)))) {
+        if (flgs.has(fi.flag) && (!fi.except_flag.has_value() || flgs.has_not(fi.except_flag.value()))) {
             add_inscription(&ptr, _(kanji ? fi.japanese : fi.english, fi.english));
         }
     }
@@ -172,7 +172,7 @@ static char *inscribe_flags_aux(std::vector<flag_insc_table> &fi_vec, const TrFl
 static bool has_flag_of(std::vector<flag_insc_table> &fi_vec, const TrFlags &flgs)
 {
     for (flag_insc_table &fi : fi_vec) {
-        if (flgs.has(fi.flag) && (fi.except_flag == -1 || flgs.has_not(i2enum<tr_type>(fi.except_flag)))) {
+        if (flgs.has(fi.flag) && (!fi.except_flag.has_value() || flgs.has_not(fi.except_flag.value()))) {
             return true;
         }
     }

sikabane-works/bakabakaband

src/flavor/flavor-util.cpp
@@ -154,7 +154,7 @@ static char *inscribe_flags_aux(std::vector<flag_insc_table> &fi_vec, const TrFl
 #endif
 
     for (flag_insc_table &fi : fi_vec) {
-        if (flags.has(fi.flag) && (fi.except_flag == -1 || flags.has_not(i2enum<tr_type>(fi.except_flag)))) {
+        if (flags.has(fi.flag) && (!fi.except_flag.has_value() || flags.has_not(fi.except_flag.value()))) {
             add_inscription(&ptr, _(kanji ? fi.japanese : fi.english, fi.english));
         }
     }
@@ -172,7 +172,7 @@ static char *inscribe_flags_aux(std::vector<flag_insc_table> &fi_vec, const TrFl
 static bool has_flag_of(std::vector<flag_insc_table> &fi_vec, const TrFlags &flags)
 {
     for (flag_insc_table &fi : fi_vec) {
-        if (flags.has(fi.flag) && (fi.except_flag == -1 || flags.has_not(i2enum<tr_type>(fi.except_flag)))) {
+        if (flags.has(fi.flag) && (!fi.except_flag.has_value() || flags.has_not(fi.except_flag.value()))) {
             return true;
         }
     }