hengband/hengband

[Refactor] #56 Renamed f_info.txt to GridFeatureDefinitions.txt

src/player-base/player-race.cpp
@@ -149,7 +149,7 @@ int16_t PlayerRace::speed() const
 
     if (this->equals(PlayerRaceType::MERFOLK)) {
         auto *floor_ptr = this->player_ptr->current_floor_ptr;
-        auto *f_ptr = &f_info[floor_ptr->grid_array[this->player_ptr->y][this->player_ptr->x].feat];
+        auto *f_ptr = &terrains_info[floor_ptr->grid_array[this->player_ptr->y][this->player_ptr->x].feat];
         if (f_ptr->flags.has(FloorFeatureType::WATER)) {
             result += (2 + this->player_ptr->lev / 10);
         } else if (!this->player_ptr->levitation) {

sikabane-works/bakabakaband

src/player-base/player-race.cpp
@@ -145,7 +145,7 @@ int16_t PlayerRace::speed() const
 
     floor_type *floor_ptr = this->player_ptr->current_floor_ptr;
     if (player_ptr->x > 0 && player_ptr->y > 0 && player_ptr->x <= floor_ptr->width - 1 && player_ptr->y <= floor_ptr->height - 1) {
-        feature_type *f_ptr = &f_info[floor_ptr->grid_array[this->player_ptr->y][this->player_ptr->x].feat];
+        terrain_type *f_ptr = &terrains_info[floor_ptr->grid_array[this->player_ptr->y][this->player_ptr->x].feat];
         if (f_ptr->flags.has(FloorFeatureType::SLOW)) {
             result -= 10;
         }