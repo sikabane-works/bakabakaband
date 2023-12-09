hengband/hengband

[Refactor] #2738 struct floor_type をclass FloorType に変えた

src/grid/grid.h
@@ -42,7 +42,7 @@ enum grid_bold_type {
     GB_SOLID_NOPERM
 };
 
-struct floor_type;
+class FloorType;
 struct grid_type;
 class PlayerType;
 struct monster_race;
@@ -58,7 +58,7 @@ void print_bolt_pict(PlayerType *player_ptr, POSITION y, POSITION x, POSITION ny
 void note_spot(PlayerType *player_ptr, POSITION y, POSITION x);
 void lite_spot(PlayerType *player_ptr, POSITION y, POSITION x);
 void update_flow(PlayerType *player_ptr);
-FEAT_IDX feat_state(floor_type *floor_ptr, FEAT_IDX feat, TerrainCharacteristics action);
+FEAT_IDX feat_state(FloorType *floor_ptr, FEAT_IDX feat, TerrainCharacteristics action);
 void cave_alter_feat(PlayerType *player_ptr, POSITION y, POSITION x, TerrainCharacteristics action);
 bool is_open(PlayerType *player_ptr, FEAT_IDX feat);
 bool check_local_illumination(PlayerType *player_ptr, POSITION y, POSITION x);
@@ -68,9 +68,9 @@ void place_grid(PlayerType *player_ptr, grid_type *g_ptr, grid_bold_type pg_type
 bool darkened_grid(PlayerType *player_ptr, grid_type *g_ptr);
 void delete_monster(PlayerType *player_ptr, POSITION y, POSITION x);
 void place_bold(PlayerType *player_ptr, POSITION y, POSITION x, grid_bold_type gh_type);
-void set_cave_feat(floor_type *floor_ptr, POSITION y, POSITION x, FEAT_IDX feature_idx);
+void set_cave_feat(FloorType *floor_ptr, POSITION y, POSITION x, FEAT_IDX feature_idx);
 int count_dt(PlayerType *player_ptr, POSITION *y, POSITION *x, bool (*test)(PlayerType *, FEAT_IDX), bool under);
-void cave_lite_hack(floor_type *floor_ptr, POSITION y, POSITION x);
-void cave_redraw_later(floor_type *floor_ptr, POSITION y, POSITION x);
-void cave_note_and_redraw_later(floor_type *floor_ptr, POSITION y, POSITION x);
-void cave_view_hack(floor_type *floor_ptr, POSITION y, POSITION x);
+void cave_lite_hack(FloorType *floor_ptr, POSITION y, POSITION x);
+void cave_redraw_later(FloorType *floor_ptr, POSITION y, POSITION x);
+void cave_note_and_redraw_later(FloorType *floor_ptr, POSITION y, POSITION x);
+void cave_view_hack(FloorType *floor_ptr, POSITION y, POSITION x);

src/view/display-map.cpp
@@ -100,7 +100,7 @@ static void image_random(TERM_COLOR *ap, char *cp)
  * 周り全てが壁に囲まれている壁についてはオプション状態による。
  * 1か所でも空きがあるか、壁ではない地形、金を含む地形、永久岩は表示。
  */
-static bool is_revealed_wall(floor_type *floor_ptr, TerrainType *f_ptr, POSITION y, POSITION x)
+static bool is_revealed_wall(FloorType *floor_ptr, TerrainType *f_ptr, POSITION y, POSITION x)
 {
     if (view_hidden_walls) {
         if (view_unsafe_walls) {

sikabane-works/bakabakaband

src/grid/grid.h
@@ -42,7 +42,7 @@ enum grid_bold_type {
     GB_SOLID_NOPERM
 };
 
-struct floor_type;
+class FloorType;
 struct grid_type;
 class PlayerType;
 struct monster_race;
@@ -58,7 +58,7 @@ void print_bolt_pict(PlayerType *player_ptr, POSITION y, POSITION x, POSITION ny
 void note_spot(PlayerType *player_ptr, POSITION y, POSITION x);
 void lite_spot(PlayerType *player_ptr, POSITION y, POSITION x);
 void update_flow(PlayerType *player_ptr);
-FEAT_IDX feat_state(floor_type *floor_ptr, FEAT_IDX feat, FloorFeatureType action);
+FEAT_IDX feat_state(FloorType *floor_ptr, FEAT_IDX feat, FloorFeatureType action);
 void cave_alter_feat(PlayerType *player_ptr, POSITION y, POSITION x, FloorFeatureType action);
 bool is_open(PlayerType *player_ptr, FEAT_IDX feat);
 bool check_local_illumination(PlayerType *player_ptr, POSITION y, POSITION x);
@@ -68,10 +68,10 @@ void place_grid(PlayerType *player_ptr, grid_type *g_ptr, grid_bold_type pg_type
 bool darkened_grid(PlayerType *player_ptr, grid_type *g_ptr);
 void delete_monster(PlayerType *player_ptr, POSITION y, POSITION x);
 void place_bold(PlayerType *player_ptr, POSITION y, POSITION x, grid_bold_type gh_type);
-void set_cave_feat(floor_type *floor_ptr, POSITION y, POSITION x, FEAT_IDX feature_idx);
-void set_cave_feat_mimic(floor_type *floor_ptr, POSITION y, POSITION x, FEAT_IDX feature_idx);
+void set_cave_feat(FloorType *floor_ptr, POSITION y, POSITION x, FEAT_IDX feature_idx);
+void set_cave_feat_mimic(FloorType *floor_ptr, POSITION y, POSITION x, FEAT_IDX feature_idx);
 int count_dt(PlayerType *player_ptr, POSITION *y, POSITION *x, bool (*test)(PlayerType *, FEAT_IDX), bool under);
-void cave_lite_hack(floor_type *floor_ptr, POSITION y, POSITION x);
-void cave_redraw_later(floor_type *floor_ptr, POSITION y, POSITION x);
-void cave_note_and_redraw_later(floor_type *floor_ptr, POSITION y, POSITION x);
-void cave_view_hack(floor_type *floor_ptr, POSITION y, POSITION x);
+void cave_lite_hack(FloorType *floor_ptr, POSITION y, POSITION x);
+void cave_redraw_later(FloorType *floor_ptr, POSITION y, POSITION x);
+void cave_note_and_redraw_later(FloorType *floor_ptr, POSITION y, POSITION x);
+void cave_view_hack(FloorType *floor_ptr, POSITION y, POSITION x);

src/view/display-map.cpp
@@ -99,7 +99,7 @@ static void image_random(TERM_COLOR *ap, char *cp)
  * 周り全てが壁に囲まれている壁についてはオプション状態による。
  * 1か所でも空きがあるか、壁ではない地形、金を含む地形、永久岩は表示。
  */
-static bool is_revealed_wall(floor_type *floor_ptr, terrain_type *f_ptr, POSITION y, POSITION x)
+static bool is_revealed_wall(FloorType *floor_ptr, terrain_type *f_ptr, POSITION y, POSITION x)
 {
     if (view_hidden_walls) {
         if (view_unsafe_walls) {