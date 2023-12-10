df7aa74a91a8bc5d561304a149b242601dd82a2a
Deskull <sikabane-works@users.noreply.github.com>
2023-12-10 22:20:20 +0900

    [Revert] 馬鹿馬鹿ではjump_floorはwizだけの仕様ではなくなっているので敢えて取り込んだうえでRevert.  "[Refactor] Moved jump_floor() from floor-leaver.cpp/h to wizard-special-process.cpp"
    This reverts commit d0ba5c06e877d4c6ca04d1454abbb198e01bb0fb.

--- src/floor/floor-leaver.h
+++ src/floor/floor-leaver.h
@@ -1,4 +1,6 @@
 ﻿#pragma once
+#include "system/angband.h"
 
 class PlayerType;
 void leave_floor(PlayerType *player_ptr);
+void jump_floor(PlayerType *player_ptr, DUNGEON_IDX dun_idx, DEPTH depth);
