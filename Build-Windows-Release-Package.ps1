hengband/hengband

[Fix] ファイル名ミスで抜けていた英語版のreadme-eng.mdがアーカイブに加わるよう修正。

Build-Windows-Release-Package.ps1
@@ -43,4 +43,4 @@ function BuildPackage ($package_name, $package_unique_files, $build_conf) {
 # 日本語版
 BuildPackage -package_name Hengband-$Version-jp -package_unique_files .\readme.md, .\autopick.txt -build_conf Release
 # 英語版
-BuildPackage -package_name Hengband-$Version-en -package_unique_files .\readme_eng.md, .\autopick_eng.txt -build_conf English-Release
+BuildPackage -package_name Hengband-$Version-en -package_unique_files .\readme-eng.md, .\autopick_eng.txt -build_conf English-Release

sikabane-works/bakabakaband

Build-Windows-Release-Package.ps1
@@ -43,4 +43,4 @@ function BuildPackage ($package_name, $package_unique_files, $build_conf) {
 # 日本語版
 BuildPackage -package_name Bakabakaband-$Version-jp -package_unique_files .\readme.md, .\autopick.txt -build_conf Release
 # 英語版
-BuildPackage -package_name Bakabakaband-$Version-en -package_unique_files .\readme_eng.md, .\autopick_eng.txt -build_conf English-Release
+BuildPackage -package_name Bakabakaband-$Version-en -package_unique_files .\readme-eng.md, .\autopick_eng.txt -build_conf English-Release