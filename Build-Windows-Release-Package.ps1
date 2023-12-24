hengband/hengband

[Fix] xtra/music内のファイルがmusic.cfg以外削除されていたので、readme.txtと*.mp3は残すように設定。

Build-Windows-Release-Package.ps1
@@ -30,7 +30,7 @@ function BuildPackage ($package_name, $package_unique_files, $build_conf) {
     Copy-Item -Verbose -Recurse -Path .\lib -Destination $hengbandDir -Exclude Makefile.am, *.raw, .gitattributes
     Copy-Item -Verbose -Path .\lib\apex\h_scores.raw -Destination $hengbandDir\lib\apex
     Remove-Item -Verbose -Exclude delete.me -Recurse -Path $hengbandDir\lib\save\*, $hengbandDir\lib\user\*
-    Remove-Item -Verbose -Exclude music.cfg -Path $hengbandDir\lib\xtra\music\*
+    Remove-Item -Verbose -Exclude music.cfg, readme.txt, *.mp3 -Path $hengbandDir\lib\xtra\music\*
 
     # zipアーカイブ作成
     $package_path = Join-Path $(Get-Location) "${package_name}.zip"

sikabane-works/bakabakaband

Build-Windows-Release-Package.ps1
@@ -30,7 +30,7 @@ function BuildPackage ($package_name, $package_unique_files, $build_conf) {
     Copy-Item -Verbose -Recurse -Path .\lib -Destination $bakabakabandDir -Exclude Makefile.am, *.raw, .gitattributes
     Copy-Item -Verbose -Path .\lib\apex\h_scores.raw -Destination $bakabakabandDir\lib\apex
     Remove-Item -Verbose -Exclude delete.me -Recurse -Path $bakabakabandDir\lib\save\*, $bakabakabandDir\lib\user\*
-    Remove-Item -Verbose -Exclude music.cfg -Path $bakabakabandDir\lib\xtra\music\*
+    Remove-Item -Verbose -Exclude music.cfg, readme.txt, *.mp3 -Path $bakabakabandDir\lib\xtra\music\*
 
     # zipアーカイブ作成
     $package_path = Join-Path $(Get-Location) "${package_name}.zip"