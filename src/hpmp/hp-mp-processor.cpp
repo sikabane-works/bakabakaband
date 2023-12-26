hengband/hengband

[Refactor] #2776 ItemEntity::tval/sval をBaseitemKey::bi_key として再定義した

src/hpmp/hp-mp-processor.cpp
@@ -150,7 +150,7 @@ void process_player_hp_mp(PlayerType *player_ptr)
         o_ptr = &player_ptr->inventory_list[INVEN_LITE];
         auto flgs = object_flags(o_ptr);
 
-        if ((player_ptr->inventory_list[INVEN_LITE].tval != ItemKindType::NONE) && flgs.has_not(TR_DARK_SOURCE) && !has_resist_lite(player_ptr)) {
+        if ((player_ptr->inventory_list[INVEN_LITE].bi_key.tval() != ItemKindType::NONE) && flgs.has_not(TR_DARK_SOURCE) && !has_resist_lite(player_ptr)) {
             GAME_TEXT o_name[MAX_NLEN];
             char ouch[MAX_NLEN + 40];
             describe_flavor(player_ptr, o_name, o_ptr, (OD_OMIT_PREFIX | OD_NAME_ONLY));

sikabane-works/bakabakaband

src/hpmp/hp-mp-processor.cpp
@@ -159,7 +159,7 @@ void process_player_hp_mp(PlayerType *player_ptr)
         o_ptr = &player_ptr->inventory_list[INVEN_LITE];
         auto flags = object_flags(o_ptr);
 
-        if ((player_ptr->inventory_list[INVEN_LITE].tval != ItemKindType::NONE) && flags.has_not(TR_DARK_SOURCE) && !has_resist_lite(player_ptr)) {
+        if ((player_ptr->inventory_list[INVEN_LITE].bi_key.tval() != ItemKindType::NONE) && flags.has_not(TR_DARK_SOURCE) && !has_resist_lite(player_ptr)) {
             GAME_TEXT o_name[MAX_NLEN];
             char ouch[MAX_NLEN + 40];
             describe_flavor(player_ptr, o_name, o_ptr, (OD_OMIT_PREFIX | OD_NAME_ONLY));