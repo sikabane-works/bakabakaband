hengband/hengband

[Refactor] #2844 *k_ptr/&k_ref を&baseitem に差し替えた (一部flavor周りで残りアリ)

src/object/object-flags.cpp
@@ -48,24 +48,20 @@ static void object_flags_lite(const ItemEntity *o_ptr, TrFlags &flgs)
  */
 TrFlags object_flags(const ItemEntity *o_ptr)
 {
-    auto *k_ptr = &baseitems_info[o_ptr->bi_id];
-
-    /* Base object */
-    auto flgs = k_ptr->flags;
+    const auto &baseitem = baseitems_info[o_ptr->bi_id];
+    auto flgs = baseitem.flags;
 
     if (o_ptr->is_fixed_artifact()) {
         flgs = artifacts_info.at(o_ptr->fixed_artifact_idx).flags;
     }
 
     object_flags_lite(o_ptr, flgs);
-
-    /* Random artifact ! */
     flgs.set(o_ptr->art_flags);
-
     if (auto effect = Smith::object_effect(o_ptr); effect.has_value()) {
         auto tr_flags = Smith::get_effect_tr_flags(effect.value());
         flgs.set(tr_flags);
     }
+
     if (Smith::object_activation(o_ptr).has_value()) {
         flgs.set(TR_ACTIVATE);
     }
@@ -82,15 +78,14 @@ TrFlags object_flags(const ItemEntity *o_ptr)
 TrFlags object_flags_known(const ItemEntity *o_ptr)
 {
     bool spoil = false;
-    auto *k_ptr = &baseitems_info[o_ptr->bi_id];
     TrFlags flgs{};
 
     if (!o_ptr->is_aware()) {
         return flgs;
     }
 
-    /* Base object */
-    flgs = k_ptr->flags;
+    const auto &baseitem = baseitems_info[o_ptr->bi_id];
+    flgs = baseitem.flags;
 
     if (!o_ptr->is_known()) {
         return flgs;

src/object/object-value-calc.cpp
@@ -21,14 +21,9 @@
 PRICE flag_cost(const ItemEntity *o_ptr, int plusses)
 {
     PRICE total = 0;
-    auto *k_ptr = &baseitems_info[o_ptr->bi_id];
     auto flgs = object_flags(o_ptr);
-
-    /*
-     * Exclude fixed flags of the base item.
-     * pval bonuses of base item will be treated later.
-     */
-    flgs.reset(k_ptr->flags);
+    const auto &baseitem = baseitems_info[o_ptr->bi_id];
+    flgs.reset(baseitem.flags);
 
     if (o_ptr->is_fixed_artifact()) {
         const auto &a_ref = artifacts_info.at(o_ptr->fixed_artifact_idx);

sikabane-works/bakabakaband

src/object/object-flags.cpp
@@ -48,24 +48,20 @@ static void object_flags_lite(const ItemEntity *o_ptr, TrFlags &flags)
  */
 TrFlags object_flags(const ItemEntity *o_ptr)
 {
-    auto *k_ptr = &baseitems_info[o_ptr->bi_id];
-
-    /* Base object */
-    auto flags = k_ptr->flags;
+    const auto &baseitem = baseitems_info[o_ptr->bi_id];
+    auto flags = baseitem.flags;
 
     if (o_ptr->is_fixed_artifact()) {
         flags = artifacts_info.at(o_ptr->fixed_artifact_idx).flags;
     }
 
     object_flags_lite(o_ptr, flags);
-
-    /* Random artifact ! */
     flags.set(o_ptr->art_flags);
-
     if (auto effect = Smith::object_effect(o_ptr); effect.has_value()) {
         auto tr_flags = Smith::get_effect_tr_flags(effect.value());
         flags.set(tr_flags);
     }
+
     if (Smith::object_activation(o_ptr).has_value()) {
         flags.set(TR_ACTIVATE);
     }
@@ -82,15 +78,14 @@ TrFlags object_flags(const ItemEntity *o_ptr)
 TrFlags object_flags_known(const ItemEntity *o_ptr)
 {
     bool spoil = false;
-    auto *k_ptr = &baseitems_info[o_ptr->bi_id];
     TrFlags flags{};
 
     if (!o_ptr->is_aware()) {
         return flags;
     }
 
-    /* Base object */
-    flags = k_ptr->flags;
+    const auto &baseitem = baseitems_info[o_ptr->bi_id];
+    flags = baseitem.flags;
 
     if (!o_ptr->is_known()) {
         return flags;

src/object/object-value-calc.cpp
@@ -21,14 +21,9 @@
 PRICE flag_cost(const ItemEntity *o_ptr, int plusses)
 {
     PRICE total = 0;
-    auto *k_ptr = &baseitems_info[o_ptr->bi_id];
     auto flags = object_flags(o_ptr);
-
-    /*
-     * Exclude fixed flags of the base item.
-     * pval bonuses of base item will be treated later.
-     */
-    flags.reset(k_ptr->flags);
+    const auto &baseitem = baseitems_info[o_ptr->bi_id];
+    flags.reset(baseitem.flags);
 
     if (o_ptr->is_fixed_artifact()) {
         const auto &a_ref = artifacts_info.at(o_ptr->fixed_artifact_idx);