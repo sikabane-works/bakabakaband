hengband/hengband

Rename put_version() to get_version(); refactor it to avoid sprintf().  Does part of the work of resolving https://github.com/hengband/hengband/issues/2858 and https://github.com/hengband/hengband/issues/2859 .

src/wizard/fixed-artifacts-spoiler.cpp
@@ -64,9 +64,7 @@ void spoiler_outlist(concptr header, concptr *list, char separator)
  */
 static void print_header(void)
 {
-    char title[180];
-    put_version(title);
-    spoiler_underline(std::string("Artifact Spoilers for Hengband Version ").append(title).data());
+    spoiler_underline(std::string("Artifact Spoilers for Hengband Version ").append(get_version()).data());
 }
 
 /*!

sikabane-works/bakabakaband

src/wizard/fixed-artifacts-spoiler.cpp
@@ -71,11 +71,7 @@ void spoiler_outlist(concptr header, concptr *list, char separator)
  */
 static void print_header(void)
 {
-    char buf[360];
-    char title[180];
-    put_version(title);
-    sprintf(buf, "Artifact Spoilers for Bakabakaband Version %s", title);
-    spoiler_underline(buf);
+    spoiler_underline(std::string("Artifact Spoilers for Bakabakaband Version ").append(get_version()).data());
 }
 
 /*!