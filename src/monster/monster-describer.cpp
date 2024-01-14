hengband/hengband

[Refactor] #3017 decide_monster_personal_pronoun() from monster_desc()

src/monster/monster-describer.cpp
@@ -20,29 +20,6 @@
 #include <string>
 #include <string_view>
 
-static std::string get_describing_monster_name(const MonsterEntity &monster, const bool is_hallucinated, const BIT_FLAGS mode)
-{
-    const auto &monrace = monraces_info[monster.ap_r_idx];
-    if (!is_hallucinated || any_bits(mode, MD_IGNORE_HALLU)) {
-        return any_bits(mode, MD_TRUE_NAME) ? monster.get_real_r_ref().name : monrace.name;
-    }
-
-    if (one_in_(2)) {
-        constexpr auto filename = _("silly_j.txt", "silly.txt");
-        const auto silly_name = get_random_line(filename, enum2i(monster.r_idx));
-        if (silly_name.has_value()) {
-            return silly_name.value();
-        }
-    }
-
-    MonsterRaceInfo *hallu_race;
-    do {
-        auto r_idx = MonsterRace::pick_one_at_random();
-        hallu_race = &monraces_info[r_idx];
-    } while (hallu_race->name.empty() || hallu_race->kind_flags.has(MonsterKindType::UNIQUE));
-    return hallu_race->name;
-}
-
 static std::string get_monster_personal_pronoun(const int kind, const BIT_FLAGS mode)
 {
     switch (kind + (mode & (MD_INDEF_HIDDEN | MD_POSSESSIVE | MD_OBJECTIVE))) {
@@ -97,6 +74,52 @@ static std::string get_monster_personal_pronoun(const int kind, const BIT_FLAGS 
     default:
         return _("何か", "it");
     }
+}
+
+static std::optional<std::string> decide_monster_personal_pronoun(const MonsterEntity *m_ptr, const BIT_FLAGS mode)
+{
+    const auto seen = (m_ptr && ((mode & MD_ASSUME_VISIBLE) || (!(mode & MD_ASSUME_HIDDEN) && m_ptr->ml)));
+    const auto pron = (m_ptr && ((seen && (mode & MD_PRON_VISIBLE)) || (!seen && (mode & MD_PRON_HIDDEN))));
+    if (seen && !pron) {
+        return std::nullopt;
+    }
+
+    const auto &monrace = monraces_info[m_ptr->ap_r_idx];
+    auto kind = 0x00;
+    if (any_bits(monrace.flags1, RF1_FEMALE)) {
+        kind = 0x20;
+    } else if (any_bits(monrace.flags1, RF1_MALE)) {
+        kind = 0x10;
+    }
+
+    if (!m_ptr || !pron) {
+        kind = 0x00;
+    }
+
+    return get_monster_personal_pronoun(kind, mode);
+}
+
+static std::string get_describing_monster_name(const MonsterEntity &monster, const bool is_hallucinated, const BIT_FLAGS mode)
+{
+    const auto &monrace = monraces_info[monster.ap_r_idx];
+    if (!is_hallucinated || any_bits(mode, MD_IGNORE_HALLU)) {
+        return any_bits(mode, MD_TRUE_NAME) ? monster.get_real_r_ref().name : monrace.name;
+    }
+
+    if (one_in_(2)) {
+        constexpr auto filename = _("silly_j.txt", "silly.txt");
+        const auto silly_name = get_random_line(filename, enum2i(monster.r_idx));
+        if (silly_name.has_value()) {
+            return silly_name.value();
+        }
+    }
+
+    MonsterRaceInfo *hallu_race;
+    do {
+        auto r_idx = MonsterRace::pick_one_at_random();
+        hallu_race = &monraces_info[r_idx];
+    } while (hallu_race->name.empty() || hallu_race->kind_flags.has(MonsterKindType::UNIQUE));
+    return hallu_race->name;
 }
 
 #ifdef JP
@@ -160,31 +183,13 @@ static std::string describe_non_pet(const PlayerType &player, const MonsterEntit
  */
 std::string monster_desc(PlayerType *player_ptr, MonsterEntity *m_ptr, BIT_FLAGS mode)
 {
-    const auto is_hallucinated = player_ptr->effects()->hallucination()->is_hallucinated();
-    const auto name = get_describing_monster_name(*m_ptr, is_hallucinated, mode);
-    const auto seen = (m_ptr && ((mode & MD_ASSUME_VISIBLE) || (!(mode & MD_ASSUME_HIDDEN) && m_ptr->ml)));
-    const auto pron = (m_ptr && ((seen && (mode & MD_PRON_VISIBLE)) || (!seen && (mode & MD_PRON_HIDDEN))));
-
-    /* First, try using pronouns, or describing hidden monsters */
-    const auto &monrace = monraces_info[m_ptr->ap_r_idx];
-    if (!seen || pron) {
-        auto kind = 0x00;
-        if (any_bits(monrace.flags1, RF1_FEMALE)) {
-            kind = 0x20;
-        } else if (any_bits(monrace.flags1, RF1_MALE)) {
-            kind = 0x10;
-        }
-
-        if (!m_ptr || !pron) {
-            kind = 0x00;
-        }
-
-        return get_monster_personal_pronoun(kind, mode);
+    const auto pronoun = decide_monster_personal_pronoun(m_ptr, mode);
+    if (pronoun.has_value()) {
+        return pronoun.value();
     }
 
-    /* Handle visible monsters, "reflexive" request */
+    const auto &monrace = monraces_info[m_ptr->ap_r_idx];
     if ((mode & (MD_POSSESSIVE | MD_OBJECTIVE)) == (MD_POSSESSIVE | MD_OBJECTIVE)) {
-        /* The monster is visible, so use its gender */
         if (any_bits(monrace.flags1, RF1_FEMALE)) {
             return _("彼女自身", "herself");
         } else if (any_bits(monrace.flags1, RF1_MALE)) {
@@ -194,6 +199,8 @@ std::string monster_desc(PlayerType *player_ptr, MonsterEntity *m_ptr, BIT_FLAGS
         }
     }
 
+    const auto is_hallucinated = player_ptr->effects()->hallucination()->is_hallucinated();
+    const auto name = get_describing_monster_name(*m_ptr, is_hallucinated, mode);
     std::string desc;
     if (m_ptr->is_pet() && !m_ptr->is_original_ap()) {
         desc = _(replace_monster_name_undefined(name), format("%s?", name.data()));

sikabane-works/bakabakaband

src/monster/monster-describer.cpp
@@ -21,29 +21,6 @@
 #include <string>
 #include <string_view>
 
-static std::string get_describing_monster_name(const MonsterEntity &monster, const bool is_hallucinated, const BIT_FLAGS mode)
-{
-    const auto &monrace = monraces_info[monster.ap_r_idx];
-    if (!is_hallucinated || any_bits(mode, MD_IGNORE_HALLU)) {
-        return any_bits(mode, MD_TRUE_NAME) ? monster.get_real_r_ref().name : monrace.name;
-    }
-
-    if (one_in_(2)) {
-        constexpr auto filename = _("silly_j.txt", "silly.txt");
-        const auto silly_name = get_random_line(filename, enum2i(monster.r_idx));
-        if (silly_name.has_value()) {
-            return silly_name.value();
-        }
-    }
-
-    MonsterRaceInfo *hallu_race;
-    do {
-        auto r_idx = MonsterRace::pick_one_at_random();
-        hallu_race = &monraces_info[r_idx];
-    } while (hallu_race->name.empty() || hallu_race->kind_flags.has(MonsterKindType::UNIQUE));
-    return hallu_race->name;
-}
-
 static std::string get_monster_personal_pronoun(const int kind, const BIT_FLAGS mode)
 {
     switch (kind + (mode & (MD_INDEF_HIDDEN | MD_POSSESSIVE | MD_OBJECTIVE))) {
@@ -98,6 +75,52 @@ static std::string get_monster_personal_pronoun(const int kind, const BIT_FLAGS 
     default:
         return _("何か", "it");
     }
+}
+
+static std::optional<std::string> decide_monster_personal_pronoun(const MonsterEntity *m_ptr, const BIT_FLAGS mode)
+{
+    const auto seen = (m_ptr && ((mode & MD_ASSUME_VISIBLE) || (!(mode & MD_ASSUME_HIDDEN) && m_ptr->ml)));
+    const auto pron = (m_ptr && ((seen && (mode & MD_PRON_VISIBLE)) || (!seen && (mode & MD_PRON_HIDDEN))));
+    if (seen && !pron) {
+        return std::nullopt;
+    }
+
+    const auto &monrace = monraces_info[m_ptr->ap_r_idx];
+    auto kind = 0x00;
+    if (any_bits(monrace.flags1, RF1_FEMALE)) {
+        kind = 0x20;
+    } else if (any_bits(monrace.flags1, RF1_MALE)) {
+        kind = 0x10;
+    }
+
+    if (!m_ptr || !pron) {
+        kind = 0x00;
+    }
+
+    return get_monster_personal_pronoun(kind, mode);
+}
+
+static std::string get_describing_monster_name(const MonsterEntity &monster, const bool is_hallucinated, const BIT_FLAGS mode)
+{
+    const auto &monrace = monraces_info[monster.ap_r_idx];
+    if (!is_hallucinated || any_bits(mode, MD_IGNORE_HALLU)) {
+        return any_bits(mode, MD_TRUE_NAME) ? monster.get_real_r_ref().name : monrace.name;
+    }
+
+    if (one_in_(2)) {
+        constexpr auto filename = _("silly_j.txt", "silly.txt");
+        const auto silly_name = get_random_line(filename, enum2i(monster.r_idx));
+        if (silly_name.has_value()) {
+            return silly_name.value();
+        }
+    }
+
+    MonsterRaceInfo *hallu_race;
+    do {
+        auto r_idx = MonsterRace::pick_one_at_random();
+        hallu_race = &monraces_info[r_idx];
+    } while (hallu_race->name.empty() || hallu_race->kind_flags.has(MonsterKindType::UNIQUE));
+    return hallu_race->name;
 }
 
 #ifdef JP
@@ -161,31 +184,13 @@ static std::string describe_non_pet(const PlayerType &player, const MonsterEntit
  */
 std::string monster_desc(PlayerType *player_ptr, MonsterEntity *m_ptr, BIT_FLAGS mode)
 {
-    const auto is_hallucinated = player_ptr->effects()->hallucination()->is_hallucinated();
-    const auto base_name = get_describing_monster_name(*m_ptr, is_hallucinated, mode);
-    const auto seen = (m_ptr && ((mode & MD_ASSUME_VISIBLE) || (!(mode & MD_ASSUME_HIDDEN) && m_ptr->ml)));
-    const auto pron = (m_ptr && ((seen && (mode & MD_PRON_VISIBLE)) || (!seen && (mode & MD_PRON_HIDDEN))));
-
-    /* First, try using pronouns, or describing hidden monsters */
-    const auto &monrace = monraces_info[m_ptr->ap_r_idx];
-    if (!seen || pron) {
-        auto kind = 0x00;
-        if (any_bits(monrace.flags1, RF1_FEMALE)) {
-            kind = 0x20;
-        } else if (any_bits(monrace.flags1, RF1_MALE)) {
-            kind = 0x10;
-        }
-
-        if (!m_ptr || !pron) {
-            kind = 0x00;
-        }
-
-        return get_monster_personal_pronoun(kind, mode);
+    const auto pronoun = decide_monster_personal_pronoun(m_ptr, mode);
+    if (pronoun.has_value()) {
+        return pronoun.value();
     }
 
-    /* Handle visible monsters, "reflexive" request */
+    const auto &monrace = monraces_info[m_ptr->ap_r_idx];
     if ((mode & (MD_POSSESSIVE | MD_OBJECTIVE)) == (MD_POSSESSIVE | MD_OBJECTIVE)) {
-        /* The monster is visible, so use its gender */
         if (any_bits(monrace.flags1, RF1_FEMALE)) {
             return _("彼女自身", "herself");
         } else if (any_bits(monrace.flags1, RF1_MALE)) {
@@ -194,6 +199,9 @@ std::string monster_desc(PlayerType *player_ptr, MonsterEntity *m_ptr, BIT_FLAGS
             return _("それ自身", "itself");
         }
     }
+
+    const auto is_hallucinated = player_ptr->effects()->hallucination()->is_hallucinated();
+    const auto base_name = get_describing_monster_name(*m_ptr, is_hallucinated, mode);
 
     std::stringstream ss;
 #ifdef JP