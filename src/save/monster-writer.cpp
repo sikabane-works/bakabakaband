ad4aee6b16ea2f73b3d783fee09568e9d0a7905d
Slimebreath6078 <slimebreath6078@yahoo.co.jp>
2024-01-29 00:08:26 +0900

    [Feature] セーブデータにその他の思い出フラグを追加

--- src/save/monster-writer.cpp
+++ src/save/monster-writer.cpp
@@ -218,6 +218,7 @@ void wr_lore(MonsterRaceId r_idx)
     wr_FlagGroup(r_ptr->r_drop_flags, wr_byte);
     wr_FlagGroup(r_ptr->r_feature_flags, wr_byte);
     wr_FlagGroup(r_ptr->r_special_flags, wr_byte);
+    wr_FlagGroup(r_ptr->r_misc_flags, wr_byte);
 
     wr_byte((byte)r_ptr->mob_num);
     wr_s16b(r_ptr->floor_id);
