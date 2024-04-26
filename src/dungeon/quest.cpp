hengband/hengband

[Refactor] #3325 no_questor_or_bounty_uniques() をMonsterRaceInfo へ繰り込んだ

src/dungeon/quest.cpp
@@ -184,14 +184,9 @@ void determine_random_questor(PlayerType *player_ptr, QuestType *q_ptr)
     get_mon_num_prep(player_ptr, mon_hook_quest, nullptr);
     MonsterRaceId r_idx;
     while (true) {
-        /*
-         * Random monster 5 - 10 levels out of depth
-         * (depending on level)
-         */
         r_idx = get_mon_num(player_ptr, 0, q_ptr->level + 5 + randint1(q_ptr->level / 10), GMN_ARENA);
         MonsterRaceInfo *r_ptr;
         r_ptr = &monraces_info[r_idx];
-
         if (r_ptr->kind_flags.has_not(MonsterKindType::UNIQUE)) {
             continue;
         }
@@ -213,7 +208,7 @@ void determine_random_questor(PlayerType *player_ptr, QuestType *q_ptr)
         if (r_ptr->wilderness_flags.has(MonsterWildernessType::WILD_ONLY)) {
             continue;
         }
-        if (no_questor_or_bounty_uniques(r_idx)) {
+        if (!r_ptr->is_suitable_questor_bounty()) {
             continue;
         }
 

src/market/bounty.cpp
@@ -329,7 +329,7 @@ void determine_bounty_uniques(PlayerType *player_ptr)
         bool is_suitable = r_ref.kind_flags.has(MonsterKindType::UNIQUE);
         is_suitable &= r_ref.drop_flags.has_any_of({ MonsterDropType::DROP_CORPSE, MonsterDropType::DROP_SKELETON });
         is_suitable &= r_ref.rarity <= 100;
-        is_suitable &= !no_questor_or_bounty_uniques(r_idx);
+        is_suitable &= r_ref.is_suitable_questor_bounty();
         return is_suitable;
     };
 

sikabane-works/bakabakaband

src/dungeon/quest.cpp
@@ -184,14 +184,9 @@ void determine_random_questor(PlayerType *player_ptr, QuestType *q_ptr)
     get_mon_num_prep(player_ptr, mon_hook_quest, nullptr);
     MonsterRaceId r_idx;
     while (true) {
-        /*
-         * Random monster 5 - 10 levels out of depth
-         * (depending on level)
-         */
         r_idx = get_mon_num(player_ptr, 0, q_ptr->level + 5 + randint1(q_ptr->level / 10), GMN_ARENA);
         MonsterRaceInfo *r_ptr;
         r_ptr = &monraces_info[r_idx];
-
         if (r_ptr->kind_flags.has_not(MonsterKindType::UNIQUE)) {
             continue;
         }
@@ -213,7 +208,7 @@ void determine_random_questor(PlayerType *player_ptr, QuestType *q_ptr)
         if (r_ptr->wilderness_flags.has(MonsterWildernessType::WILD_ONLY)) {
             continue;
         }
-        if (no_questor_or_bounty_uniques(r_idx)) {
+        if (!r_ptr->no_suitable_questor_bounty()) {
             continue;
         }
 

src/market/bounty.cpp
@@ -338,7 +338,7 @@ void determine_bounty_uniques(PlayerType *player_ptr)
         bool is_suitable = r_ref.kind_flags.has(MonsterKindType::UNIQUE);
         is_suitable &= r_ref.drop_flags.has_any_of({ MonsterDropType::DROP_CORPSE, MonsterDropType::DROP_SKELETON });
         is_suitable &= r_ref.rarity <= 100;
-        is_suitable &= !no_questor_or_bounty_uniques(r_idx);
+        is_suitable &= r_ref.no_suitable_questor_bounty();
         return is_suitable;
     };
 