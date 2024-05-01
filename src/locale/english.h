256e7bafe88a86d622459dbd9606c1f6836cf302
Habu <habu1010+github@gmail.com>
2024-05-01 23:51:02 +0900

    [Refactor] plural_aux() のシグニチャの改善
    plural_aux() は英語版のみで使用される、名詞の複数形変化を処理する関数。
    引数で与えられた文字列バッファを直接変更するインターフェスから、引数で与えられた
    文字列を複数形にした新規の std::string オブジェクトを返すインターフェースへ変更する。

--- src/locale/english.h
+++ src/locale/english.h
@@ -1,4 +1,9 @@
 #pragma once
 
-void plural_aux(char *Name);
+#include <string>
+#include <string_view>
+
+#ifndef JP
+std::string pluralize(std::string_view name);
 bool is_a_vowel(int ch);
+#endif
