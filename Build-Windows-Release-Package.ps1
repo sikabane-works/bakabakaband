hengband/hengband

Revert "Merge pull request #3437 from habu1010/feature/use-cpp-httplib-for-report-score"
This reverts commit 13726d88537eeed5a12b73c7d8c87b35a839c673, reversing
changes made to 5174c0c75be8ce18eecb9780f02162ac05afaba0.

Build-Windows-Release-Package.ps1
@@ -25,7 +25,7 @@ function BuildPackage ($package_name, $package_unique_files, $build_conf) {
     New-Item $hengbandDir -ItemType Directory
 
     # 必要なファイルをコピーして、その中で不要になりえるものを削除
-    Copy-Item -Verbose -Path .\Hengband.exe, .\readme_angband, .\THIRD-PARTY-NOTICES.txt -Destination $hengbandDir
+    Copy-Item -Verbose -Path .\Hengband.exe, .\readme_angband -Destination $hengbandDir
     Copy-Item -Verbose -Path $package_unique_files -Destination $hengbandDir
     Copy-Item -Verbose -Recurse -Path .\lib -Destination $hengbandDir -Exclude Makefile.am, *.raw, .gitattributes
     Copy-Item -Verbose -Path .\lib\apex\h_scores.raw -Destination $hengbandDir\lib\apex

sikabane-works/bakabakaband

Build-Windows-Release-Package.ps1
@@ -25,7 +25,7 @@ function BuildPackage ($package_name, $package_unique_files, $build_conf) {
     New-Item $bakabakabandDir -ItemType Directory
 
     # 必要なファイルをコピーして、その中で不要になりえるものを削除
-    Copy-Item -Verbose -Path .\Bakabakaband.exe, .\readme_angband, .\THIRD-PARTY-NOTICES.txt -Destination $hengbandDir
+    Copy-Item -Verbose -Path .\Bakabakaband.exe, .\readme_angband -Destination $hengbandDir
     Copy-Item -Verbose -Path $package_unique_files -Destination $hengbandDir
     Copy-Item -Verbose -Recurse -Path .\lib -Destination $hengbandDir -Exclude Makefile.am, *.raw, .gitattributes
     Copy-Item -Verbose -Path .\lib\apex\h_scores.raw -Destination $hengbandDir\lib\apex