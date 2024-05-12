48e7a7b320eb47ea1b842d398d01735d360fb90f
Habu <habu1010+github@gmail.com>
2024-05-12 12:57:28 +0900

    [Feature] デバッグコマンドのアイテム生成にCandidateSelectorを使用する

--- src/wizard/tval-descriptions-table.cpp
+++ src/wizard/tval-descriptions-table.cpp
@@ -4,7 +4,7 @@
 /*!
  * ベースアイテムの大項目IDの種別名定義 / A list of tvals and their textual names
  */
-const std::vector<tval_desc> tvals = {
+const std::vector<tval_desc> tval_desc_list = {
     { ItemKindType::SWORD, _("刀剣", "Sword") },
     { ItemKindType::POLEARM, _("長柄/斧", "Polearm") },
     { ItemKindType::HAFTED, _("鈍器", "Hafted Weapon") },
@@ -59,15 +59,4 @@ const std::vector<tval_desc> tvals = {
     { ItemKindType::MATERIAL, _("素材", "Matrial") },
     { ItemKindType::TRAP, _("解除済罠", "Disarmed Trap") },
     { ItemKindType::NONE, nullptr },
-};
-
-/*!
- * 選択処理用キーコード /
- * Global array for converting numbers to a logical list symbol
- */
-const std::vector<char> listsym = {
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    '\0'
 };
