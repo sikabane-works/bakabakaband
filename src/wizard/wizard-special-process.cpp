hengband/hengband

[Refactor] #3496 optional 型の値取得処理 value() を撤廃した その7

src/wizard/wizard-special-process.cpp
@@ -597,18 +597,17 @@ void wiz_jump_to_dungeon(PlayerType *player_ptr)
         return;
     }
 
-    const auto level_opt = select_debugging_floor(floor, *dungeon_id);
-    if (!level_opt) {
+    const auto level = select_debugging_floor(floor, *dungeon_id);
+    if (!level) {
         return;
     }
 
-    const auto level = level_opt.value();
-    msg_format("You jump to dungeon level %d.", level);
+    msg_format("You jump to dungeon level %d.", *level);
     if (autosave_l) {
         do_cmd_save_game(player_ptr, true);
     }
 
-    wiz_jump_floor(player_ptr, *dungeon_id, level);
+    wiz_jump_floor(player_ptr, *dungeon_id, *level);
 }
 
 /*!

sikabane-works/bakabakaband

src/wizard/wizard-special-process.cpp
@@ -610,18 +610,17 @@ void wiz_jump_to_dungeon(PlayerType *player_ptr)
         return;
     }
 
-    const auto level_opt = select_debugging_floor(floor, *dungeon_id);
-    if (!level_opt) {
+    const auto level = select_debugging_floor(floor, *dungeon_id);
+    if (!level) {
         return;
     }
 
-    const auto level = level_opt.value();
-    msg_format("You jump to dungeon level %d.", level);
+    msg_format("You jump to dungeon level %d.", *level);
     if (autosave_l) {
         do_cmd_save_game(player_ptr, true);
     }
 
-    jump_floor(player_ptr, dungeon_id.value(), level);
+    jump_floor(player_ptr, *dungeon_id, *level);
 }
 
 /*!