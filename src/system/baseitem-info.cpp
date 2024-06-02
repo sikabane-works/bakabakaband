hengband/hengband

[Refactor] #3772 ItemEntity::is_readable() の実体をBaseitemKey::is_readable() に移した
一部の固定アーティファクトは読めるようフラグが拡張されているのでそれはItemEntity 側に残した

src/system/baseitem-info.cpp
@@ -556,6 +556,13 @@ bool BaseitemKey::is_lance() const
     return is_lance;
 }
 
+bool BaseitemKey::is_readable() const
+{
+    auto can_read = this->is(ItemKindType::SCROLL);
+    can_read |= this->is(ItemKindType::PARCHMENT);
+    return can_read;
+}
+
 bool BaseitemKey::is_mushrooms() const
 {
     if (!this->subtype_value) {

sikabane-works/bakabakaband

src/system/baseitem-info.cpp
@@ -556,6 +556,13 @@ bool BaseitemKey::is_lance() const
     return is_lance;
 }
 
+bool BaseitemKey::is_readable() const
+{
+    auto can_read = this->is(ItemKindType::SCROLL);
+    can_read |= this->is(ItemKindType::READING_MATTER);
+    return can_read;
+}
+
 bool BaseitemKey::is_mushrooms() const
 {
     if (!this->subtype_value.has_value()) {