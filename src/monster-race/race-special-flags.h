Slimebreath6078/hengband

[Implement] 特殊フラグを実装

src/monster-race/race-special-flags.h
@@ -1,0 +1,6 @@
+#pragma once
+
+enum class MonsterSpecialType {
+    LAFFEY_II, /*!< ラフィーIIの軽減特性 */
+    MAX,
+};

sikabane-works/bakabakaband

src/monster-race/race-special-flags.h
@@ -1,0 +1,6 @@
+#pragma once
+
+enum class MonsterSpecialType {
+    DIMINISH_MAX_DAMAGE, /*!< ラフィーIIの軽減特性 */
+    MAX,
+};