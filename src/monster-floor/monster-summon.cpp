hengband/hengband

[Refactor] summon_specificで使用するグローバル変数の除去
summon_specific()で使用する以下のグローバル変数を除去し、引数で渡すように変更する。
- summon_specific_who
- summon_specific_type
- summon_unique_okay

src/monster-floor/monster-summon.cpp
@@ -21,25 +21,14 @@
 #include "system/player-type-definition.h"
 
 /*!
- * @var summon_specific_who
- * @brief 召喚を行ったプレイヤーあるいはモンスターのIDを示すグローバル変数 / Hack -- the index of the summoning monster
- * @todo summon_specific_who グローバル変数の除去と関数引数への代替を行う
- */
-int summon_specific_who = -1;
-
-/*!
- * @var summon_unique_okay
- * @brief 召喚対象にユニークを含めるかを示すグローバル変数 / summoning unique enable
- * @todo summon_unique_okay グローバル変数の除去と関数引数への代替を行う
- */
-bool summon_unique_okay = false;
-
-/*!
  * @brief モンスターが召喚の基本条件に合っているかをチェックする / Hack -- help decide if a monster race is "okay" to summon
  * @param r_idx チェックするモンスター種族ID
+ * @param summon_who 召喚主のモンスター情報ID
+ * @param type 召喚種別
+ * @param unique_okay ユニークモンスターを召喚対象に含めるかどうか
  * @return 召喚対象にできるならばTRUE
  */
-static bool summon_specific_okay(PlayerType *player_ptr, MonsterRaceId r_idx)
+static bool summon_specific_okay(PlayerType *player_ptr, MonsterRaceId r_idx, MONSTER_IDX summon_who, summon_type type, bool unique_okay)
 {
     auto *r_ptr = &monraces_info[r_idx];
     if (!mon_hook_dungeon(player_ptr, r_idx)) {
@@ -47,27 +36,27 @@ static bool summon_specific_okay(PlayerType *player_ptr, MonsterRaceId r_idx)
     }
 
     auto &floor = *player_ptr->current_floor_ptr;
-    if (summon_specific_who > 0) {
-        auto *m_ptr = &floor.m_list[summon_specific_who];
+    if (summon_who > 0) {
+        auto *m_ptr = &floor.m_list[summon_who];
         if (monster_has_hostile_align(player_ptr, m_ptr, 0, 0, r_ptr)) {
             return false;
         }
-    } else if (summon_specific_who < 0) {
+    } else if (summon_who < 0) {
         if (monster_has_hostile_align(player_ptr, nullptr, 10, -10, r_ptr) && !one_in_(std::abs(player_ptr->alignment) / 2 + 1)) {
             return false;
         }
     }
 
-    if (!summon_unique_okay && (r_ptr->kind_flags.has(MonsterKindType::UNIQUE) || (r_ptr->population_flags.has(MonsterPopulationType::NAZGUL)))) {
+    if (!unique_okay && (r_ptr->kind_flags.has(MonsterKindType::UNIQUE) || (r_ptr->population_flags.has(MonsterPopulationType::NAZGUL)))) {
         return false;
     }
 
-    if (!summon_specific_type) {
+    if (type == SUMMON_NONE) {
         return true;
     }
 
     const auto is_like_unique = r_ptr->kind_flags.has(MonsterKindType::UNIQUE) || (r_ptr->population_flags.has(MonsterPopulationType::NAZGUL));
-    if ((summon_specific_who < 0) && is_like_unique && monster_has_hostile_align(player_ptr, nullptr, 10, -10, r_ptr)) {
+    if ((summon_who < 0) && is_like_unique && monster_has_hostile_align(player_ptr, nullptr, 10, -10, r_ptr)) {
         return false;
     }
 
@@ -75,11 +64,11 @@ static bool summon_specific_okay(PlayerType *player_ptr, MonsterRaceId r_idx)
         return true;
     }
 
-    if (summon_specific_who > 0) {
-        auto *m_ptr = &floor.m_list[summon_specific_who];
-        return check_summon_specific(player_ptr, m_ptr->r_idx, r_idx);
+    if (summon_who > 0) {
+        auto *m_ptr = &floor.m_list[summon_who];
+        return check_summon_specific(player_ptr, m_ptr->r_idx, r_idx, type);
     } else {
-        return check_summon_specific(player_ptr, MonsterRaceId::PLAYER, r_idx);
+        return check_summon_specific(player_ptr, MonsterRaceId::PLAYER, r_idx, type);
     }
 }
 
@@ -137,15 +126,14 @@ bool summon_specific(PlayerType *player_ptr, MONSTER_IDX src_idx, POSITION y1, P
         return false;
     }
 
-    summon_specific_who = src_idx;
-    summon_specific_type = type;
-    summon_unique_okay = (mode & PM_ALLOW_UNIQUE) != 0;
-    get_mon_num_prep(player_ptr, summon_specific_okay, get_monster_hook2(player_ptr, y, x));
+    auto summon_specific_hook = [src_idx, type, unique_okay = (mode & PM_ALLOW_UNIQUE) != 0](PlayerType *player_ptr, MonsterRaceId r_idx) {
+        return summon_specific_okay(player_ptr, r_idx, src_idx, type, unique_okay);
+    };
+    get_mon_num_prep(player_ptr, std::move(summon_specific_hook), get_monster_hook2(player_ptr, y, x));
 
     DEPTH dlev = get_dungeon_or_wilderness_level(player_ptr);
     MonsterRaceId r_idx = get_mon_num(player_ptr, 0, (dlev + lev) / 2 + 5, mode);
     if (!MonsterRace(r_idx).is_valid()) {
-        summon_specific_type = SUMMON_NONE;
         return false;
     }
 
@@ -153,12 +141,10 @@ bool summon_specific(PlayerType *player_ptr, MONSTER_IDX src_idx, POSITION y1, P
         mode |= PM_NO_KAGE;
     }
 
-    if (!place_specific_monster(player_ptr, src_idx, y, x, r_idx, mode)) {
-        summon_specific_type = SUMMON_NONE;
+    const auto summon_who = is_monster(src_idx) ? std::make_optional(src_idx) : std::nullopt;
+    if (!place_specific_monster(player_ptr, src_idx, y, x, r_idx, mode, summon_who)) {
         return false;
     }
-
-    summon_specific_type = SUMMON_NONE;
 
     bool notice = false;
     if (!is_monster(src_idx)) {

sikabane-works/bakabakaband

src/monster-floor/monster-summon.cpp
@@ -21,25 +21,14 @@
 #include "system/player-type-definition.h"
 
 /*!
- * @var summon_specific_who
- * @brief 召喚を行ったプレイヤーあるいはモンスターのIDを示すグローバル変数 / Hack -- the index of the summoning monster
- * @todo summon_specific_who グローバル変数の除去と関数引数への代替を行う
- */
-int summon_specific_who = -1;
-
-/*!
- * @var summon_unique_okay
- * @brief 召喚対象にユニークを含めるかを示すグローバル変数 / summoning unique enable
- * @todo summon_unique_okay グローバル変数の除去と関数引数への代替を行う
- */
-bool summon_unique_okay = false;
-
-/*!
  * @brief モンスターが召喚の基本条件に合っているかをチェックする / Hack -- help decide if a monster race is "okay" to summon
  * @param r_idx チェックするモンスター種族ID
+ * @param summon_who 召喚主のモンスター情報ID
+ * @param type 召喚種別
+ * @param is_unique_allowed ユニークモンスターを召喚対象に含めるかどうか
  * @return 召喚対象にできるならばTRUE
  */
-static bool summon_specific_okay(PlayerType *player_ptr, MonsterRaceId r_idx)
+static bool summon_specific_okay(PlayerType *player_ptr, MonsterRaceId r_idx, MONSTER_IDX summon_who, summon_type type, bool is_unique_allowed)
 {
     auto *r_ptr = &monraces_info[r_idx];
     if (!mon_hook_dungeon(player_ptr, r_idx)) {
@@ -47,27 +36,27 @@ static bool summon_specific_okay(PlayerType *player_ptr, MonsterRaceId r_idx)
     }
 
     auto &floor = *player_ptr->current_floor_ptr;
-    if (summon_specific_who > 0) {
-        auto *m_ptr = &floor.m_list[summon_specific_who];
+    if (summon_who > 0) {
+        auto *m_ptr = &floor.m_list[summon_who];
         if (monster_has_hostile_align(player_ptr, m_ptr, 0, 0, r_ptr)) {
             return false;
         }
-    } else if (summon_specific_who < 0) {
+    } else if (summon_who < 0) {
         if (monster_has_hostile_align(player_ptr, nullptr, 10, -10, r_ptr) && !one_in_(std::abs(player_ptr->alignment) / 2 + 1)) {
             return false;
         }
     }
 
-    if (!summon_unique_okay && (r_ptr->kind_flags.has(MonsterKindType::UNIQUE) || (r_ptr->population_flags.has(MonsterPopulationType::NAZGUL)))) {
+    if (!is_unique_allowed && (r_ptr->kind_flags.has(MonsterKindType::UNIQUE) || (r_ptr->population_flags.has(MonsterPopulationType::NAZGUL)))) {
         return false;
     }
 
-    if (!summon_specific_type) {
+    if (type == SUMMON_NONE) {
         return true;
     }
 
     const auto is_like_unique = r_ptr->kind_flags.has(MonsterKindType::UNIQUE) || (r_ptr->population_flags.has(MonsterPopulationType::NAZGUL));
-    if ((summon_specific_who < 0) && is_like_unique && monster_has_hostile_align(player_ptr, nullptr, 10, -10, r_ptr)) {
+    if ((summon_who < 0) && is_like_unique && monster_has_hostile_align(player_ptr, nullptr, 10, -10, r_ptr)) {
         return false;
     }
 
@@ -75,11 +64,11 @@ static bool summon_specific_okay(PlayerType *player_ptr, MonsterRaceId r_idx)
         return true;
     }
 
-    if (summon_specific_who > 0) {
-        auto *m_ptr = &floor.m_list[summon_specific_who];
-        return check_summon_specific(player_ptr, m_ptr->r_idx, r_idx);
+    if (summon_who > 0) {
+        auto *m_ptr = &floor.m_list[summon_who];
+        return check_summon_specific(player_ptr, m_ptr->r_idx, r_idx, type);
     } else {
-        return check_summon_specific(player_ptr, MonsterRaceId::PLAYER, r_idx);
+        return check_summon_specific(player_ptr, MonsterRaceId::PLAYER, r_idx, type);
     }
 }
 
@@ -137,15 +126,14 @@ bool summon_specific(PlayerType *player_ptr, MONSTER_IDX src_idx, POSITION y1, P
         return false;
     }
 
-    summon_specific_who = src_idx;
-    summon_specific_type = type;
-    summon_unique_okay = (mode & PM_ALLOW_UNIQUE) != 0;
-    get_mon_num_prep(player_ptr, summon_specific_okay, get_monster_hook2(player_ptr, y, x));
+    auto summon_specific_hook = [src_idx, type, is_unique_allowed = (mode & PM_ALLOW_UNIQUE) != 0](PlayerType *player_ptr, MonsterRaceId r_idx) {
+        return summon_specific_okay(player_ptr, r_idx, src_idx, type, is_unique_allowed);
+    };
+    get_mon_num_prep(player_ptr, std::move(summon_specific_hook), get_monster_hook2(player_ptr, y, x));
 
     DEPTH dlev = get_dungeon_or_wilderness_level(player_ptr);
     MonsterRaceId r_idx = get_mon_num(player_ptr, 0, (dlev + lev) / 2 + 5, mode);
     if (!MonsterRace(r_idx).is_valid()) {
-        summon_specific_type = SUMMON_NONE;
         return false;
     }
 
@@ -153,12 +141,10 @@ bool summon_specific(PlayerType *player_ptr, MONSTER_IDX src_idx, POSITION y1, P
         mode |= PM_NO_KAGE;
     }
 
-    if (!place_specific_monster(player_ptr, src_idx, y, x, r_idx, mode)) {
-        summon_specific_type = SUMMON_NONE;
+    const auto summon_who = is_monster(src_idx) ? std::make_optional(src_idx) : std::nullopt;
+    if (!place_specific_monster(player_ptr, src_idx, y, x, r_idx, mode, summon_who)) {
         return false;
     }
-
-    summon_specific_type = SUMMON_NONE;
 
     bool notice = false;
     if (!is_monster(src_idx)) {