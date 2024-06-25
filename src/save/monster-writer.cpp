6307b0cea7348d11e3f14d229f39dade56bdd76d
Deskull <sikabane-works@users.noreply.github.com>
2024-06-25 20:36:49 +0900

    (Monster)[Fix] モブ数保存＆読込エラー修正。セーブファイル番号26に更新。

--- src/save/monster-writer.cpp
+++ src/save/monster-writer.cpp
@@ -217,7 +217,7 @@ void wr_lore(MonsterRaceId r_idx)
     wr_FlagGroup(r_ptr->r_special_flags, wr_byte);
     wr_FlagGroup(r_ptr->r_misc_flags, wr_byte);
 
-    wr_byte((byte)r_ptr->mob_num);
+    wr_s16b(r_ptr->mob_num);
     wr_s16b(r_ptr->floor_id);
 
     wr_s16b(r_ptr->defeat_level);
