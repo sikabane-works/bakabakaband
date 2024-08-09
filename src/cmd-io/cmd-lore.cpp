hengband/hengband

[Refactor] #4112 MonsterRaceInfo::d_attr/d_char をColoredChar にまとめた

src/cmd-io/cmd-lore.cpp
@@ -137,7 +137,7 @@ void do_cmd_query_symbol(PlayerType *player_ptr)
                 monraces.push_back(monrace_id);
         }
 
-        else if (all || (monrace.d_char == symbol)) {
+        else if (all || (monrace.cc_def.character == symbol)) {
             monraces.push_back(monrace_id);
         }
     }

sikabane-works/bakabakaband

src/cmd-io/cmd-lore.cpp
@@ -137,7 +137,7 @@ void do_cmd_query_symbol(PlayerType *player_ptr)
                 monraces.push_back(monrace_id);
         }
 
-        else if (all || (monrace.d_char == sym)) {
+        else if (all || (monrace.cc_def.character == sym_opt)) {
             monraces.push_back(monrace_id);
         }
     }