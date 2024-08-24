hengband/hengband

[Refactor] PlayerType::arena_number をArenaEntryList に繰り込んだ

src/monster-floor/monster-death.cpp
@@ -82,14 +82,14 @@ static void on_defeat_arena_monster(PlayerType *player_ptr, MonsterDeath *md_ptr
 
     w_ptr->set_arena(true);
     auto &entries = ArenaEntryList::get_instance();
-    const auto max_entries = entries.get_max_entries();
-    if (player_ptr->arena_number > max_entries) {
+    const auto is_true_victor = entries.is_player_true_victor();
+    if (is_true_victor) {
         msg_print(_("素晴らしい！君こそ真の勝利者だ。", "You are a Genuine Champion!"));
     } else {
         msg_print(_("勝利！チャンピオンへの道を進んでいる。", "Victorious! You're on your way to becoming Champion."));
     }
 
-    const auto &arena = arena_info[player_ptr->arena_number];
+    const auto &arena = arena_info[entries.get_current_entry()];
     const auto tval = arena.key.tval();
     if (tval > ItemKindType::NONE) {
         ItemEntity item(arena.key);
@@ -97,17 +97,17 @@ static void on_defeat_arena_monster(PlayerType *player_ptr, MonsterDeath *md_ptr
         (void)drop_near(player_ptr, &item, -1, md_ptr->md_y, md_ptr->md_x);
     }
 
-    if (player_ptr->arena_number > max_entries) {
-        player_ptr->arena_number++;
+    if (is_true_victor) {
+        entries.increment_entry();
     }
 
-    player_ptr->arena_number++;
+    entries.increment_entry();
     if (!record_arena) {
         return;
     }
 
     const auto m_name = monster_desc(player_ptr, md_ptr->m_ptr, MD_WRONGDOER_NAME);
-    exe_write_diary(floor, DiaryKind::ARENA, player_ptr->arena_number, m_name);
+    exe_write_diary(floor, DiaryKind::ARENA, 0, m_name);
 }
 
 static void drop_corpse(PlayerType *player_ptr, MonsterDeath *md_ptr)

src/player/player-damage.cpp
@@ -376,11 +376,14 @@ int take_hit(PlayerType *player_ptr, int damage_type, int damage, std::string_vi
         }
 
         if (floor.inside_arena) {
-            const auto &m_name = monraces_info[arena_info[player_ptr->arena_number].r_idx].name;
+            auto &entries = ArenaEntryList::get_instance();
+            entries.set_defeated_entry();
+            const auto current_entry = entries.get_current_entry();
+            const auto &m_name = monraces_info[arena_info[current_entry].r_idx].name;
             msg_format(_("あなたは%sの前に敗れ去った。", "You are beaten by %s."), m_name.data());
             msg_print(nullptr);
             if (record_arena) {
-                exe_write_diary(floor, DiaryKind::ARENA, -1 - player_ptr->arena_number, m_name);
+                exe_write_diary(floor, DiaryKind::ARENA, 0, m_name);
             }
         } else {
             const auto q_idx = floor.get_quest_id();

sikabane-works/bakabakaband

src/monster-floor/monster-death.cpp
@@ -83,14 +83,14 @@ static void on_defeat_arena_monster(PlayerType *player_ptr, MonsterDeath *md_ptr
 
     w_ptr->set_arena(true);
     auto &entries = ArenaEntryList::get_instance();
-    const auto max_entries = entries.get_max_entries();
-    if (player_ptr->arena_number > max_entries) {
+    const auto is_true_victor = entries.is_player_true_victor();
+    if (is_true_victor) {
         msg_print(_("素晴らしい！君こそ真の勝利者だ。", "You are a Genuine Champion!"));
     } else {
         msg_print(_("勝利！チャンピオンへの道を進んでいる。", "Victorious! You're on your way to becoming Champion."));
     }
 
-    const auto &arena = arena_info[player_ptr->arena_number];
+    const auto &arena = arena_info[entries.get_current_entry()];
     const auto tval = arena.key.tval();
     if (tval > ItemKindType::NONE) {
         ItemEntity item;
@@ -99,17 +99,17 @@ static void on_defeat_arena_monster(PlayerType *player_ptr, MonsterDeath *md_ptr
         (void)drop_near(player_ptr, &item, -1, md_ptr->md_y, md_ptr->md_x);
     }
 
-    if (player_ptr->arena_number > max_entries) {
-        player_ptr->arena_number++;
+    if (is_true_victor) {
+        entries.increment_entry();
     }
 
-    player_ptr->arena_number++;
+    entries.increment_entry();
     if (!record_arena) {
         return;
     }
 
     const auto m_name = monster_desc(player_ptr, md_ptr->m_ptr, MD_WRONGDOER_NAME);
-    exe_write_diary(player_ptr, DiaryKind::ARENA, player_ptr->arena_number, m_name);
+    exe_write_diary(player_ptr, DiaryKind::ARENA, 0, m_name);
 }
 
 static void drop_corpse(PlayerType *player_ptr, MonsterDeath *md_ptr)

src/player/player-damage.cpp
@@ -379,11 +379,14 @@ int take_hit(PlayerType *player_ptr, int damage_type, int damage, std::string_vi
 
         const auto &floor = *player_ptr->current_floor_ptr;
         if (floor.inside_arena) {
-            const auto &m_name = monraces_info[arena_info[player_ptr->arena_number].r_idx].name;
+            auto &entries = ArenaEntryList::get_instance();
+            entries.set_defeated_entry();
+            const auto current_entry = entries.get_current_entry();
+            const auto &m_name = monraces_info[arena_info[current_entry].r_idx].name;
             msg_format(_("あなたは%sの前に敗れ去った。", "You are beaten by %s."), m_name.data());
             msg_print(nullptr);
             if (record_arena) {
-                exe_write_diary(player_ptr, DiaryKind::ARENA, -1 - player_ptr->arena_number, m_name);
+                exe_write_diary(player_ptr, DiaryKind::ARENA, 0, m_name);
             }
         } else {
             const auto q_idx = floor.get_quest_id();