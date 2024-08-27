hengband/hengband

[Refactor] 死体と骨のを表すItemKindTypeの列挙値の名称を変更
ソースコード上での紛らわしさを解消するため、ItemKindTypeの列挙値について
以下の改名を行う。

- SKELETON -> FLAVOR_SKELETON
- CORPSE -> MONSTER_REMAINS

src/wizard/spoiler-table.cpp
@@ -40,9 +40,9 @@ const std::vector<grouper> group_item_list = {
 
     { { ItemKindType::FIGURINE }, _("人形", "Magical Figurines") },
     { { ItemKindType::STATUE }, _("像", "Statues") },
-    { { ItemKindType::CORPSE }, _("死体", "Corpses") },
+    { { ItemKindType::MONSTER_REMAINS }, _("死体", "Corpses") },
 
-    { { ItemKindType::SKELETON, ItemKindType::BOTTLE, ItemKindType::JUNK, ItemKindType::SPIKE, ItemKindType::FLASK, ItemKindType::PARCHMENT }, _("その他", "Misc") },
+    { { ItemKindType::FLAVOR_SKELETON, ItemKindType::BOTTLE, ItemKindType::JUNK, ItemKindType::SPIKE, ItemKindType::FLASK, ItemKindType::PARCHMENT }, _("その他", "Misc") },
 };
 
 /* The artifacts categorized by type */

sikabane-works/bakabakaband

src/wizard/spoiler-table.cpp
@@ -40,9 +40,9 @@ const std::vector<grouper> group_item_list = {
 
     { { ItemKindType::FIGURINE }, _("人形", "Magical Figurines") },
     { { ItemKindType::STATUE }, _("像", "Statues") },
-    { { ItemKindType::CORPSE }, _("死体", "Corpses") },
+    { { ItemKindType::MONSTER_REMAINS }, _("死体", "Corpses") },
 
-    { { ItemKindType::SKELETON, ItemKindType::BOTTLE, ItemKindType::JUNK, ItemKindType::SPIKE, ItemKindType::FLASK, ItemKindType::READING_MATTER }, _("その他", "Misc") },
+    { { ItemKindType::FLAVOR_SKELETON, ItemKindType::BOTTLE, ItemKindType::JUNK, ItemKindType::SPIKE, ItemKindType::FLASK, ItemKindType::READING_MATTER }, _("その他", "Misc") },
 };
 
 /* The artifacts categorized by type */