hengband/hengband

[Refactor] chameleon_change_m_idx を削除する

src/monster-floor/place-monster-types.h
@@ -20,5 +20,6 @@ enum place_monster_type {
     PM_NO_QUEST = 0x00001000, /*!< モンスター生成フラグ: クエスト除外モンスターを生成しない */
     PM_CLONE = 0x00002000, /*!< モンスター生成フラグ: クローンとして生成する */
     PM_ARENA = 0x00004000, /*!< モンスター生成フラグ: アリーナ用の生成 */
+    PM_CHAMELEON = 0x00008000, /*!< モンスター生成フラグ: カメレオンとして生成する */
 
 };

sikabane-works/bakabakaband

src/monster-floor/place-monster-types.h
@@ -21,5 +21,6 @@ enum place_monster_type {
     PM_AMBUSH = 0x00002000, /*!< モンスター生成フラグ: 1回の白兵攻撃を行ってくる */
     PM_CLONE = 0x00004000, /*!< モンスター生成フラグ: クローンとして生成する */
     PM_ARENA = 0x00008000, /*!< モンスター生成フラグ: アリーナ用の生成 */
+    PM_CHAMELEON = 0x00010000, /*!< モンスター生成フラグ: カメレオンとして生成する */
 
 };