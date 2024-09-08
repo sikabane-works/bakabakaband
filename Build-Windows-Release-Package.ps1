hengband/hengband

[Chore] Win用リリースパッケージにHengband.pdbを追加
エラーレポートに出力するコールスタック情報を取得するため、プログラム
データベースファイルHengband.pdbをWin用リリースパッケージに含める。

Build-Windows-Release-Package.ps1
@@ -25,7 +25,7 @@ function BuildPackage ($package_name, $package_unique_files, $build_conf) {
     New-Item $hengbandDir -ItemType Directory
 
     # 必要なファイルをコピーして、その中で不要になりえるものを削除
-    Copy-Item -Verbose -Path .\Hengband.exe, .\readme_angband, .\THIRD-PARTY-NOTICES.txt -Destination $hengbandDir
+    Copy-Item -Verbose -Path .\Hengband.exe, .\Hengband.pdb, .\readme_angband, .\THIRD-PARTY-NOTICES.txt -Destination $hengbandDir
     Copy-Item -Verbose -Path $package_unique_files -Destination $hengbandDir
     Copy-Item -Verbose -Recurse -Path .\lib -Destination $hengbandDir -Exclude Makefile.am, *.raw, .gitattributes
     Copy-Item -Verbose -Path .\lib\apex\h_scores.raw -Destination $hengbandDir\lib\apex

sikabane-works/bakabakaband

Build-Windows-Release-Package.ps1
@@ -25,7 +25,7 @@ function BuildPackage ($package_name, $package_unique_files, $build_conf) {
     New-Item $bakabakabandDir -ItemType Directory
 
     # 必要なファイルをコピーして、その中で不要になりえるものを削除
-    Copy-Item -Verbose -Path .\Bakabakaband.exe, .\readme_angband, .\THIRD-PARTY-NOTICES.txt -Destination $bakabakabandDir
+    Copy-Item -Verbose -Path .\Bakabakaband.exe, .\Bakabakaband.pdb, .\readme_angband, .\THIRD-PARTY-NOTICES.txt -Destination $bakabakabandDir
     Copy-Item -Verbose -Path $package_unique_files -Destination $bakabakabandDir
     Copy-Item -Verbose -Recurse -Path .\lib -Destination $bakabakabandDir -Exclude Makefile.am, *.raw, .gitattributes
     Copy-Item -Verbose -Path .\lib\apex\h_scores.raw -Destination $bakabakabandDir\lib\apex